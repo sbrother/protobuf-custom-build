5a54a0502026ee7bf2e4ee02eb7b9785e60ed811
Sam Brotherton <sbrother@gmail.com>
2022-02-14 22:16:51 -0700

    Remove protol step

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # protobuf-custom-build
 
 This is a small package that allows a repository containing only protobuf
-definitions to be used in a Python project via `pip install`. It uses the
-excellent [protoletariat](https://github.com/cpcloud/protoletariat) library
-under the hood. Please see
+definitions to be used in a Python project via `pip install`. Please see
 [python-protobuf-repo-examle](https://github.com/sbrother/python-protobuf-repo-example)
 for a full example of a protobuf repo using this package; it boils down to
 including the line
--- custom/build.py
+++ custom/build.py
@@ -11,18 +11,10 @@ class Command(setuptools.Command):
         pass
 
     def run(self):
-        log.info("Running protobuf compilation...")
+        log.info("Running protobuf compilation step...")
         all_protos = [f for f in setuptools.findall() if f.endswith('.proto')]
         if not all_protos:
             return
 
-        root_dir = os.path.commonpath(all_protos)
-        log.info(f"Compiling protos in {root_dir}...")
-        subprocess.run(['protoc', '--python_out=' + root_dir, '-I' + root_dir]
-                       + all_protos, check=True)
-
-        log.info(f"Running protoletariat to fix imports...")
-        subprocess.run(['protol', '--create-package', '--in-place',
-                        '--python-out', root_dir, 'protoc',
-                        f'--proto-path={root_dir}'] + all_protos,
-                       check=True)
+        log.info(f"Compiling protos in {os.path.commonpath(all_protos)}...")
+        subprocess.run(['protoc', '--python_out=.'] + all_protos, check=True)
